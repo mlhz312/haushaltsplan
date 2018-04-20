cf48ee5d38a0fe5345c2718779fe8638e06e984b
Michael <michael.heinitz@adidas.com>
2018-04-20 16:17:34 +0200

    Parts of header finishe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { TableViewComponent } from './table-view/table-view.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    TableViewComponent
   ],
   imports: [
     BrowserModule
